84e934ed297f769c744bdca890883a71e09b7572
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-07-05 10:36:47 -0700

    Updated resume and resume link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export class Footer extends Component {
                     <a href="https://github.com/vsan21" rel="noopener noreferrer" target="_blank"><i class="fab fa-github fa-3x fa-fw"></i></a>
                     <a href="https://www.linkedin.com/in/viviansan/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin fa-3x fa-fw"></i></a>
                     <a href="https://twitter.com/VivianSan21" rel="noopener noreferrer" target="_blank"><i class="fab fa-twitter fa-3x fa-fw"></i></a>
-                    <a href="https://drive.google.com/file/d/1b1aTifk7F9Rq7drWADFQ_IftHmbGYMfx/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i class="fas fa-file-invoice fa-3x fa-fw"></i></a>
+                    <a href="https://drive.google.com/file/d/108VCn4J6sKof2nd4cVfVb32DyA7I5k6v/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i class="fas fa-file-invoice fa-3x fa-fw"></i></a>
                 </div>
                 <br />
                 <p>Made with <i class="fas fa-heart"></i> by Vivian San</p>
