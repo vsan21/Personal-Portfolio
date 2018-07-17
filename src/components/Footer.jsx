462f62095da369efd5aab578a0944764e1a339c9
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-07-16 17:10:08 -0700

    website is now responsive on multiple screen sizes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ export class Footer extends Component {
         return (
             <footer id='footer-container'>
                 <div id="footer-links">
-                    <a href="https://github.com/vsan21" rel="noopener noreferrer" target="_blank"><i class="fab fa-github fa-3x fa-fw"></i></a>
-                    <a href="https://www.linkedin.com/in/viviansan/" rel="noopener noreferrer" target="_blank"><i class="fab fa-linkedin fa-3x fa-fw"></i></a>
-                    <a href="https://twitter.com/VivianSan21" rel="noopener noreferrer" target="_blank"><i class="fab fa-twitter fa-3x fa-fw"></i></a>
-                    <a href="https://drive.google.com/file/d/108VCn4J6sKof2nd4cVfVb32DyA7I5k6v/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i class="fas fa-file-invoice fa-3x fa-fw"></i></a>
+                    <a href="https://github.com/vsan21" rel="noopener noreferrer" target="_blank"><i className="fab fa-github fa-3x fa-fw"></i></a>
+                    <a href="https://www.linkedin.com/in/viviansan/" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin fa-3x fa-fw"></i></a>
+                    <a href="https://twitter.com/VivianSan21" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter fa-3x fa-fw"></i></a>
+                    <a href="https://drive.google.com/file/d/108VCn4J6sKof2nd4cVfVb32DyA7I5k6v/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i className="fas fa-file-invoice fa-3x fa-fw"></i></a>
                 </div>
                 <br />
-                <p>Made with <i class="fas fa-heart"></i> by Vivian San</p>
+                <p>Made with <i className="fas fa-heart"></i> by Vivian San</p>
             </footer>
         );
     }
