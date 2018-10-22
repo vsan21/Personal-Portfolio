025beaad68d2056dff90bb9438609a6d1770aa4d
Vivian San <viviansan@Vivians-MacBook-Air.local>
2018-10-21 23:44:54 -0700

    Updated resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export class Footer extends Component {
                     <a href="https://github.com/vsan21" rel="noopener noreferrer" target="_blank"><i className="fab fa-github fa-3x fa-fw"></i></a>
                     <a href="https://www.linkedin.com/in/viviansan/" rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin fa-3x fa-fw"></i></a>
                     <a href="https://twitter.com/VivianSan21" rel="noopener noreferrer" target="_blank"><i className="fab fa-twitter fa-3x fa-fw"></i></a>
-                    <a href="https://drive.google.com/file/d/1qAoFftPgq37hHkDNlDuOvAvqKx9eiQhU/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i className="fas fa-file-invoice fa-3x fa-fw"></i></a>
+                    <a href="https://drive.google.com/file/d/1G3ngrbD8P8jr7XqN5C3Hu28P_2lXoSxv/view?usp=sharing" rel="noopener noreferrer" target="_blank"><i className="fas fa-file-invoice fa-3x fa-fw"></i></a>
                 </div>
                 <br />
                 <p>Made by Viv's <span role="img" aria-label="Brain">🧠</span> </p>
